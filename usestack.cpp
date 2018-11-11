3513d5e5e6ab58341614cfc7fc25eff2b5fee506
Scott Matsubara <smatsubara@cs.ucsb.edu>
2018-11-10 18:47:47 -0800

    first commit

---
+++ usestack.cpp
@@ -0,0 +1,29 @@
+// usestack.cpp - for CS 24 lab practice using stacks
+// NAME(S), DATE
+
+#include "intstack.h"
+#include <iostream>
+using namespace std;
+//Evaluating "10 20 5 / *"
+int main() {
+  
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(5);
+
+    int right=s.top();
+    s.pop();
+    int left=s.top();
+    s.pop();
+    s.push(left/right);
+
+    right=s.top();
+    s.pop();
+    left=s.top();
+    s.pop();
+    s.push(left*right);
+
+    cout << s.top() << endl;
+}
